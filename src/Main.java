14da0cab812718e431b4745fae802017cd74cd8a
Aníbal Morales <gonibal15@gmail.com>
2026-05-29 14:40:33 -0600

    Código limpio y comentado.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -21,7 +19,7 @@ public class Main {
         entrada.close();
 
         System.out.println("--- Datos ingresados ---");
-        System.out.println("Datos "+nombre+" "+edad+" "+profesion);
+        System.out.println("Datos: "+nombre+" Edad: "+edad+" Profesión: "+profesion);
 
     }
 }
